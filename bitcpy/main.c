7ed1b07fd956dc9d7b70793d92c521ad6620821a
that1guy <thatguysemail007@gmail.com>
2020-03-15 21:05:14 -0400

    changes, and updates to explore memcpy function to replicate

--- bitcpy/main.c
+++ bitcpy/main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void* myBitCopy(void* toLocation, const void* fromLocation, size_t numOfBits) {
-    size_t numOfBytes = numOfBits / 8; // what is size_t
-    int paddingRequired = numOfBits % 8;
+void* myBitCopy(void* toLocation, const void* fromLocation, int len_of_mem_to_copy) {
+    size_t numOfBytes = len_of_mem_to_copy / 8; 
+    int paddingRequired = len_of_mem_to_copy % 8;
+    
+    //I need to think about how we handle not just strings and if there any cases that need to be handled for my implementation of memcpy
+    
 }
 
 int main() {
-    printf("wtfd\n");
+    char* characterString = "Th1s 1s 4 Ch4r STRING";
+    char* toLoc = (char* ) malloc(sizeof(characterString));
+    memcpy(toLoc, characterString, strlen(characterString));
+    printf("%s : %d characters : %d bytes\n", toLoc, strlen(characterString), sizeof(characterString));
+    free(toLoc);
+    return (0);
 }
